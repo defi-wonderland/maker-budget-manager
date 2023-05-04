fbe6bda7b92415de088ba66dd980436618e24617
Weißer Hase <wei3erHase@protonmail.com>
2023-05-04 19:35:08 +0200

    chore: deployment

--- deploy/001_deploy.ts
+++ deploy/001_deploy.ts
@@ -5,7 +5,7 @@ import { shouldVerifyContract } from '../utils/deploy';
 const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
 
-  const wonderland = '0x45fEEBbd5Cf86dF61be8F81025E22Ae07a07cB23';
+  const wonderland = '0xa211CA28299D9caaD3d6A050C98f393726B1F39e';
   const args = [wonderland];
 
   const deploy = await hre.deployments.deploy('MakerDAOBudgetManager', {
